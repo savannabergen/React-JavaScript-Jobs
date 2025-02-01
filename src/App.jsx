9cbbee827ade7e30222a55795928177f68614170
Savanna Bergen <savanna.bergen@gmail.com>
2025-02-01 13:09:58 -0600

    back to work

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
+const name = 'Savanna';
+const styles = {
+  color: 'red',
+  fontSize: '55px'
+}
+
 const App = () => {
   return (
-    <div className='text-5xl'>
-      App
-    </div>
+    <>
+      <div className='text-5xl'>App</div>
+      <p style={styles}>hello {name}</p>
+    </>
   )
 }
 
